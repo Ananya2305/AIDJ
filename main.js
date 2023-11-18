3eeb8a8232db291da917ea644644e73229cb3b91
Ananya2305 <planetsneha.shukla@gmail.com>
2023-11-18 14:24:50 +0530

    firstcommit

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 song="";
+rightWristX=0;
+rightWristY=0;
+leftWristX=0;
+leftWristY=0;
 function preload(){
     song=loadSound("music.mp3")
 }
@@ -11,6 +15,27 @@ function setup()
     video=createCapture(VIDEO);
     video.hide();
 
+    poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPoses);
+
+
+}
+function modelLoaded(){
+    console.log('poseNet is initialized');
+}
+
+function gotPoses(results){
+    if(results.length>0)
+    {
+        console.log(results);
+        leftWristX=results[0].pose.leftWrist.x;
+        leftWristY=result[0].pose.leftWrist.y;
+        console.log("leftWristX="+leftWristX+"leftWristY="+leftWristY);
+        rightWristX=result[0].pose.rightWrist.x;
+        rightWristY=result[0].pose.rightwrist.y;
+        console.log("rightWristX="+rightWristX+"rightWristY="+rightWristY);
+
+    }
 }
 function draw(){
     image(video,0,0,600,500);
@@ -18,4 +43,7 @@ function draw(){
 }
 function play(){
     song.play();
+    song.setVolume(1);
+    song.rate(1);
+
 }

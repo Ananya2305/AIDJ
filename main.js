80e2be92aa3738e54dbbdc8aa853f8a4135b83ac
Ananya2305 <planetsneha.shukla@gmail.com>
2023-11-24 15:37:52 +0530

    comppleting main.js

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ rightWristY = 0;
 leftWristX = 0;
 leftWristY = 0;
 scoreLeftWrist = 0;
+scoreRightWrist= 0;
+
 
 function preload() {
     song = loadSound("music.mp3")
@@ -30,6 +32,8 @@ function gotPoses(results) {
         console.log(results);
         scoreLeftWrist = results[0].pose.keypoints[9].score;
         console.log("scoreLeftWrist=" + scoreLeftWrist);
+        scorerightWrist = results[0].pose.keypoints[10].score;
+        console.log("scorerightWrist=" + scorerightWrist);
 
         leftWristX = results[0].pose.leftWrist.x;
         leftWristY = result[0].pose.leftWrist.y;
@@ -38,12 +42,43 @@ function gotPoses(results) {
         rightWristY = result[0].pose.rightwrist.y;
         console.log("rightWristX=" + rightWristX + "rightWristY=" + rightWristY);
 
+
     }
 }
 function draw() {
     image(video, 0, 0, 600, 500);
     fill("#FF0000");
     stroke("#FF0000");
+    if(scorerightWrist>0.2){
+        circle(rightWristX,rightWristY,20);
+        if(rightWristY>0 && rightWristY<=100){
+            document.getElementById("speed").innerHTML="speed 0.5x";
+            song.rate(0.5);
+    
+        }
+        else if(rightWristY>100 && rightWristY<=200){
+            document.getElementById("speed").innerHTML="speed 1x";
+            song.rate(1);
+            
+        }
+        else if(rightWristY>200 && rightWristY<=300){
+            document.getElementById("speed").innerHTML="speed 1.5x";
+            song.rate(1.5);
+            
+        }
+        else if(rightWristY>300 && rightWristY<=400){
+            document.getElementById("speed").innerHTML="speed 2x";
+            song.rate(2);
+           
+        }
+        else if(rightWristY>400 && rightWristY<=500){
+            document.getElementById("speed").innerHTML="speed 2.5x";
+            song.rate(2.5);
+        
+            
+        }
+    }
+    
     if (scoreLeftWrist > 0.2) {
         circle(leftWristX, leftWristY, 20);
         InNumberLeftWristY = Number(leftWristY);
